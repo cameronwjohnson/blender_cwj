6dbd168eac33afe248080d10f3a3f5da613761c8
cameronwjohnson <cwj@uoregon.edu>
2020-01-05 13:13:20 -0800

    added functions

--- stgr_spiral.py
+++ stgr_spiral.py
@@ -1,138 +1,58 @@
 import bpy, bmesh
 import numpy as np
+import blender_defs as bd
+import time as tm
+import sys
+from progress_bar import progress
+
 
 for o in bpy.data.objects:
-    bpy.data.objects.remove(bpy.data.objects[o.name],True)
+    bd.remove(o.name)
 
+r = 3
+th = r*0.02
+domename = 'dome'
+bd.make_dome(r,th,domename)
 
-scale = 5
+h = r*2.5 # cone height
+size = 0.15 # (0.125) size of each cone
+v = 4 # verticies
+psc = 4 # point scale
+spsc = {3:1.25, # spacing scale
+        4:1.5,
+        5:1.75}
 
-r = 2*scale
-rth = 0.125*scale
+points = []
+nn = 11**2 # number of stars
+sa = 1.6   # (2.26)  angular scale for spiral
+sr = 1.125 # (1.125) radial scale for spiral
+i0 = 0.1   # intital offset
+for j in range(nn):
+    si = np.sqrt(j+i0)
+    loc = [sr*si*np.cos(sa*si*np.pi),sr*si*np.sin(sa*si*np.pi)]
 
-chout = np.sqrt(2)*(r/2+rth)*0.995
-chin = np.sqrt(2)*(r/2)*0.831
-ch = r*(1+np.sqrt(2)/2)
+    points.append(int(np.random.random()*3+3))
+    rang = np.random.random()*np.pi
+    rrv = np.random.random()*0.4+0.6
+    for i in range(points[-1]):
+        bd.cone(size,h,v,'star{}{}'.format(j,i),rot=(np.pi,0,0))
+        for vert in bpy.data.objects['star{}{}'.format(j,i)].data.vertices:
+            if vert.co[2]<0:
+                if abs(vert.co[1])>size/2 and np.sign(vert.co[1])==1:
+                    vert.co[1]*=psc
+                    vert.co[1]+=spsc[points[-1]]*size
+                else:
+                    vert.co[1]+=spsc[points[-1]]*size
+                ang = np.arctan2(vert.co[1],vert.co[0])
+                rv = np.sqrt(vert.co[1]**2+vert.co[0]**2)
+                vert.co[0] = rrv*rv*np.cos(ang+rang+i*2*np.pi/points[-1]) + loc[0]
+                vert.co[1] = rrv*rv*np.sin(ang+rang+i*2*np.pi/points[-1]) + loc[1]
 
-subdiv = 6
+t0 = tm.clock()
+bd.active(domename)
+for j in range(nn):
+    for i in range(points[j]):
+        bd.difference('star{}{}'.format(j,i))
+        bd.remove('star{}{}'.format(j,i))
 
-bpy.ops.mesh.primitive_ico_sphere_add(size=r+rth,subdivisions=subdiv, view_align=False, enter_editmode=False, location=(0, 0, r), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.context.selected_objects[0].name = 'sph'
-bpy.ops.mesh.primitive_ico_sphere_add(size=r,subdivisions=subdiv, view_align=False, enter_editmode=False, location=(0, 0, r), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.context.selected_objects[0].name = 'sphex'
-
-bpy.ops.mesh.primitive_cylinder_add(vertices=64, radius=r+rth, depth=r, view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.context.selected_objects[0].name = 'base'
-
-bpy.ops.mesh.primitive_cone_add(vertices=256, radius1=chin, depth=chin, view_align=False, enter_editmode=False, location=(0,0,ch+chout/2), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.context.selected_objects[0].name = 'conein'
-
-bpy.ops.mesh.primitive_cone_add(vertices=256, radius1=chout, depth=chout, view_align=False, enter_editmode=False, location=(0,0,ch+chout/2), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.context.selected_objects[0].name = 'coneout'
-
-bpy.context.scene.objects.active = bpy.data.objects['sph']
-
-bpy.ops.object.modifier_add(type='BOOLEAN')
-bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['base']
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-
-bpy.ops.object.modifier_add(type='BOOLEAN')
-bpy.context.object.modifiers["Boolean"].operation = 'UNION'
-bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['coneout']
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-
-bpy.ops.object.modifier_add(type='BOOLEAN')
-bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['sphex']
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-
-bpy.ops.object.modifier_add(type='BOOLEAN')
-bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['conein']
-bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-
-bpy.data.objects.remove(bpy.data.objects['base'],True)
-bpy.data.objects.remove(bpy.data.objects['sphex'],True)
-bpy.data.objects.remove(bpy.data.objects['coneout'],True)
-bpy.data.objects.remove(bpy.data.objects['conein'],True)
-
-#
-# h = r*3
-# b = 0.36*scale
-# sp = b*2
-# nn = 13**2
-# vs = 10
-# locs = []
-#
-# sa = 2.26
-# sr = 1.5
-# i0 = 0.1
-# for i in range(nn):
-#     si = np.sqrt(i+i0)
-#     locs.append([sr*sp*si*0.75*np.cos(sa*si*np.pi),sr*sp*si*0.75*np.sin(sa*si*np.pi)])
-#
-#
-#
-# for i in range(nn):
-#
-#     bpy.ops.mesh.primitive_cone_add(vertices=vs, radius1=b, depth=h, view_align=False, enter_editmode=False, location=(locs[i][0],locs[i][1], h/2), rotation=(0,np.pi,0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-#     bpy.context.selected_objects[0].name = 'cone{}'.format(i)
-#     type='star'
-#
-#
-#     mm=0
-#     randsize = np.random.random()*0.4+0.6
-#     randphi = np.random.random()*2*np.pi/5
-#     for vert in bpy.data.objects['cone{}'.format(i)].data.vertices:
-#         if vert.co[2]>0:
-#             vert.co[0]+=locs[i][0]
-#             vert.co[1]+=-locs[i][1]
-#         else:
-#             if (np.arctan2(vert.co[1],vert.co[0])+np.pi)%(2*np.pi/(vs/2))<2*np.pi/vs:
-#                 vert.co[0]*=0.5*randsize
-#                 vert.co[1]*=0.5*randsize
-#             else:
-#                 vert.co[0]*=randsize
-#                 vert.co[1]*=randsize
-#             vertr = np.sqrt(vert.co[0]**2+vert.co[1]**2)
-#             vertphi = np.arctan2(vert.co[0],vert.co[1])+randphi
-#             vert.co[0] = vertr*np.cos(vertphi)
-#             vert.co[1] = vertr*np.sin(vertphi)
-#         mm+=1
-#
-#     bpy.context.scene.objects.active = bpy.data.objects['sph']
-#
-#     bpy.ops.object.modifier_add(type='BOOLEAN')
-#     bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-#     bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['cone{}'.format(i)]
-#     bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-#
-# mloc = [1.2*sp,-0.55*sp]
-# rot = 18*np.pi/16
-#
-# bpy.ops.mesh.primitive_cone_add(vertices=4*vs, radius1=2.5*b, depth=h, view_align=False, enter_editmode=False, location=(mloc[0],mloc[1], h/2), rotation=(0,np.pi,0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-# bpy.context.selected_objects[0].name = 'cone{}'.format(nn)
-#
-# for vert in bpy.data.objects['cone{}'.format(nn)].data.vertices:
-#     if vert.co[2]>0:
-#         vert.co[0]+=mloc[0]
-#         vert.co[1]+=-mloc[1]
-#     else:
-#         if vert.co[0]<0:
-#             vert.co[0]*=-0.65
-#         vertr = np.sqrt(vert.co[0]**2+vert.co[1]**2)
-#         vertphi = np.arctan2(vert.co[0],vert.co[1])+rot
-#         vert.co[0] = vertr*np.cos(vertphi)
-#         vert.co[1] = vertr*np.sin(vertphi)
-#
-# bpy.context.scene.objects.active = bpy.data.objects['sph']
-#
-# bpy.ops.object.modifier_add(type='BOOLEAN')
-# bpy.context.object.modifiers["Boolean"].operation = 'DIFFERENCE'
-# bpy.context.object.modifiers["Boolean"].object = bpy.data.objects['cone{}'.format(nn)]
-# bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
-#
-# for i in range(nn+1):
-#     bpy.data.objects.remove(bpy.data.objects['cone{}'.format(i)],True)
-# bpy.context.scene.update()
+    progress(j,nn,t0,inc10=False)
